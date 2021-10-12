d801d9a543bbb68cefa9bed1d39237da0859837a
Justin Gries <justin.gries@gmail.com>
2021-10-12 16:57:15 -0700

    Initial stubbed out code for 9.2

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * Date 2021-10-12
+ * Coder: Justin Gries
+ * Tester(s):
+ *
+ * Prompt: Project 9.2
+ *  A Person has a name (just a first name for simplicity) and friends
+ *  .  Store the names of the friends in a string, separated by spaces.
+ *  Provide a constructor that constructs a person with a given name and no
+ *  friends.
+ *  Provide member functions
+        void befriend(Person p)
+        void unfriend(Person p)
+        string get_friend_names()
+ *
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
