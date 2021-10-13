f165f51d7bc8251e20cb5269a4b7a80673d3afd2
Justin Gries <justin.gries@gmail.com>
2021-10-12 18:10:33 -0700

    More information in the project description Removal of argument testing

--- main.cpp
+++ main.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 /*
+ * Course: Chaffey College 2021-Fall CS2
+ * Prof: Dr. Tracy Kocher
  * Date 2021-10-12
  * Coder: Justin Gries
  * Tester(s):
  *
  * Prompt: Project 9.2
- *  A Person has a name (just a first name for simplicity) and friends
- *  .  Store the names of the friends in a string, separated by spaces.
- *  Provide a constructor that constructs a person with a given name and no
- *  friends.
- *  Provide member functions
-        void befriend(Person p)
-        void unfriend(Person p)
-        string get_friend_names()
+ *  A Person has a name (just a first name for simplicity) and friends.
+ *      * Store the names of the friends in a string, separated by spaces.
+ *      * Provide a constructor that constructs a person with a given name
+ *          and no friends.
+ *      * Provide member functions
+            void befriend(Person p)
+            void unfriend(Person p)
+            string get_friend_names()
  *
  */
-int main(int argc, char *argv[]) {
-    for(int i = 0; i < argc; i++)
-        cout << argv[i] << endl;
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
     return 0;
 }
